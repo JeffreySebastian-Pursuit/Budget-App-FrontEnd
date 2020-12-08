2947c208362246a4abb0511188d95f223b43b665
Jeffrey <jeffreysebastian@pursuit.org>
2020-12-08 14:08:34 -0500

    variables

---
+++ valuesTypesOperators/module1_javascript.js
@@ -0,0 +1,89 @@
+// JS numbers can be floats (decimals), positive, negative, and etch....
+// can do math with number
+// NaN (Not a number) is of type Number
+// modulo gives you the remainder of the number.
+// double equal sign checks if the number and what's inside the string are equal. triple equal sign checks for value.
+
+// String being added to each other is called string concatenation
+// we can add strings. strings added to anything become a string.
+// backticks(``), double quotes(""), and single quotes ('').
+
+//Booleans are true or false. they are their own data types.
+
+// Coparrison Operations
+// === - triple equesls for strict equality match . evaluates to a boolean.
+// !== - Not equal to
+// Also have <, >, <=, >= for comparisons
+
+// Logical operator 
+// ! mean Not
+// && AND  (are both sides truthy?) both sides needs to be truthy if not it will give you false.
+// || OR (at least one side truthy) the last thing that the computer read, its what printed in the computer.
+
+// things are Truthy or falsy.
+// falsy vaules in JS:
+// 0 /-0
+// false
+// NaN
+// null
+// undefined
+// ""
+
+// these will become super important when dealing with conditionals.
+
+"use strict" // cannot accidenetally declares a global value.
+// const person = "Jeffrey"
+// console.log(person) // will thorw error w/o using const/ strict
+//variables
+// creating a variable consists of a few parts.
+// 1. declaration - const, let var
+// 2. variable namedescribe what they are (think nouns)
+// 3. assingment of value = (assignment operator) - on the right side give value
+
+// forward slash is considered an escape key.
+// command slash for comments
+ // if you forget to declare a variable it will automatically become global
+// first_name - snakecse - common with languanges like Ruby
+// in JS we use camelCase - starts lowercase, each additional word capital
+
+// let firstName = "Jeffrey";
+// let lastName = "Sebastian";
+// console.log(lastName) 
+// lastName = "Tanasootr" // variable re-assignment operator // you cannot reassign a constant variable.
+
+// console.log(lastName) 
+
+//const city ="New York City"; // cannot change the value of the city due to const.
+//city = "Chicago";
+//console.log(city);
+
+//console.log(firstName + " " + lastName);
+
+//const animal; // declared but not assigned are undefined  and this will not work for const.
+
+//console.log(animal);
+
+// let animal = null; // null is the absense of any value.
+// console.log(animal)
+
+//let age = 100;
+//console.log(age)
+
+
+//age = age + 1;
+//age += 1 //^ is synonymous - this called syntactic sugar
+//console.log(age)
+
+//let num = 7;
+//num++; /// num = num -2, /2, *2, ++
+//console.log(num);
+
+// const favoriteNum = 13;
+// const secondfavNum = 44;
+
+// let sumOfTopTwoNuns = favoriteNum + secondfavNum
+// console.log(sumOfTopTwoNuns)
+
+// what is state? - everything that's stored in memory while a program runs.
+// 
+
